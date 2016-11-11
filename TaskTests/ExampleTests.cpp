9847e8dc04b73efb5d5516ada9e6459b7af9284f
mtso <mtso@tuta.io>
2016-11-11 14:02:09 -0800

    Create solution directory skeleton

---
+++ TaskTests/ExampleTests.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+// #include "[ObjectName].h" where [ObjectMain] is the name of the class file.
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TaskTests
+{		
+	TEST_CLASS(ExampleTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+			Assert::Fail(L"Test case has not been written yet.");
+		}
+
+	};
+}
