0c0cbfadf2babe07709d623d61d122e65d8fc0e0
mtso <mtso@tuta.io>
2016-11-15 23:44:48 -0800

    Create Sha1.h

---
+++ Task/Utilities/Sha1.h
@@ -0,0 +1,16 @@
+// Hashbrown.h
+// Task
+// CIS 22C F2016: Matthew Tso
+
+#ifndef TASK_SHA1_H
+#define TASK_SHA1_H
+
+#include <string>
+
+namespace task {
+    
+    // Returns a 40 hex-char string of the input's SHA1 hash.
+    std::string sha_1(const std::string& input);
+}
+
+#endif /* Hashbrown.h */
