5d2a9c0a29b2eeac4c78b86b12611550bc92c94d
mtso <mtso@tuta.io>
2016-11-16 08:37:00 -0800

    Add sha1 files to project

--- Task/Utilities/Sha1.h
+++ Task/Utilities/Sha1.h
@@ -6,6 +6,7 @@
 #define TASK_SHA1_H
 
 #include <string>
+#include <stdint.h>
 
 namespace task {
     
--- TaskTests/HashTests.cpp
+++ TaskTests/HashTests.cpp
@@ -3,6 +3,7 @@
 #include "CppUnitTest.h" // Unit testing API
 
 #include "Utilities/Sha1.h"
+#include <string>
 
 // Namespace of Assert::
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -15,17 +16,17 @@ namespace TaskTests // Testing Project namespace
 		
 		TEST_METHOD(Sha1_ATest)
 		{
-			string input = "A Test";
-      string expected = "8f0c0855915633e4a7de19468b3874c8901df043";
-      string actual = task::sha_1(input);
+			std::string input = "A Test";
+			std::string expected = "8f0c0855915633e4a7de19468b3874c8901df043";
+			std::string actual = task::sha_1(input);
 			Assert::AreEqual(expected, actual);
 		}
 
     TEST_METHOD(Sha1_QuickFox)
 		{
-			string input = "The quick brown fox jumped over a lazy dog.";
-      string expected = "e26d11465308c2c78f0914c69f2001cb3c454c9f";
-      string actual = task::sha_1(input);
+			std::string input = "The quick brown fox jumped over a lazy dog.";
+			std::string expected = "e26d11465308c2c78f0914c69f2001cb3c454c9f";
+			std::string actual = task::sha_1(input);
 			Assert::AreEqual(expected, actual);
 		}
 	};
