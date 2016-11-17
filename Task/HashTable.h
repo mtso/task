ee3c36f6816f36488134e0caee55b9bf4c3cecce
mtso <mtso@tuta.io>
2016-11-17 13:17:20 -0800

    Add HashTable header

---
+++ Task/HashTable.h
@@ -0,0 +1,23 @@
+// HashTable.h
+// Task
+// CIS 22C F2016: Matthew Tso
+
+#ifndef TASK_HASHTABLE_H
+#define TASK_HASHTABLE_H
+
+#include <iterator>
+#include <string>
+using namespace std;
+
+/**
+ * Hash Table
+ *
+ * Collision resolution using separate linked-list chaining.
+ */
+template <string Key, typename Value>
+class HashTable
+{
+
+};
+
+#endif
