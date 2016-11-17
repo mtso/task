40328271f61591494a376d4e887b81406217f45d
mtso <mtso@tuta.io>
2016-11-17 00:26:37 -0800

    Comment on algorithm

--- Task/Utilities/Sha1.h
+++ Task/Utilities/Sha1.h
@@ -1,6 +1,13 @@
 // Sha1.h
 // Task
 // CIS 22C F2016: Matthew Tso
+//
+// SHA-1 function that takes a string as input and
+// returns a string containing the input's hash
+// represented as 40 hex characters.
+//
+// The algorithm is taken from:
+// http://www.metamorphosite.com/one-way-hash-encryption-sha1-data-software
 
 #ifndef TASK_SHA1_H
 #define TASK_SHA1_H
