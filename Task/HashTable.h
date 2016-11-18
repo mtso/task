fb722e3d86699441c9cfd4d0cc9b933b4fd368ad
mtso <mtso@tuta.io>
2016-11-18 00:03:56 -0800

    Move djb2 comment block to Djb2.h

--- Task/HashTable.h
+++ Task/HashTable.h
@@ -2,26 +2,7 @@
 // Task
 // CIS 22C F2016: Matthew Tso
 //
-//
 // Maybe try djb2 hash algorithm from: http://www.cse.yorku.ca/~oz/hash.html
-
-/*
-djb2
-
-this algorithm (k=33) was first reported by dan bernstein many years ago in comp.lang.c. another version of this algorithm (now favored by bernstein) uses xor: hash(i) = hash(i - 1) * 33 ^ str[i]; the magic of number 33 (why it works better than many other constants, prime or not) has never been adequately explained.
-
-unsigned long
-hash(unsigned char *str)
-{
-    unsigned long hash = 5381;
-    int c;
-    
-    while (c = *str++)
-        hash = ((hash << 5) + hash) + c; // hash * 33 + c
-    
-    return hash;
-}
-/**/
 
 #ifndef TASK_DICTIONARY_H
 #define TASK_DICTIONARY_H
--- Task/Utilities/Djb2.h
+++ Task/Utilities/Djb2.h
@@ -6,6 +6,24 @@
 //  Copyright © 2016 De Anza. All rights reserved.
 //
 
+/*
+ djb2
+ 
+ this algorithm (k=33) was first reported by dan bernstein many years ago in comp.lang.c. another version of this algorithm (now favored by bernstein) uses xor: hash(i) = hash(i - 1) * 33 ^ str[i]; the magic of number 33 (why it works better than many other constants, prime or not) has never been adequately explained.
+ 
+ unsigned long
+ hash(unsigned char *str)
+ {
+ unsigned long hash = 5381;
+ int c;
+ 
+ while (c = *str++)
+ hash = ((hash << 5) + hash) + c; // hash * 33 + c
+ 
+ return hash;
+ }
+/**/
+
 #ifndef TASK_DJB2_H
 #define TASK_DJB2_H
 
