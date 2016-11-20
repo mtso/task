9619cf3d3519b15b0828cf72a5a1d1e33a4d4ffe
mtso <mtso@tuta.io>
2016-11-20 15:00:22 -0800

    Add demo directory generator header

---
+++ TaskApp/DemoDataGenerator.h
@@ -0,0 +1,12 @@
+
+
+#include "TaskEntry.h"
+
+namespace taskapp {
+
+	void generateDemoDirectory(
+		const string& demo_user, 
+		const uint64_t& demo_start_time, 
+		const uint64_t& demo_end_time, 
+		const int& entry_count);
+}
