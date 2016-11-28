9a3a138e99e563c8fd2aa3271eea82a88e7ab399
CodeTortoise <marroquin.age@gmail.com>
2016-11-28 01:49:22 -0800

    Begin writing TaskInterface, which translates user input at console and calls appropriate EntryManager functions.

--- TaskApp/TaskInterface.cpp
+++ TaskApp/TaskInterface.cpp
@@ -1,38 +1,12 @@
 #include "TaskInferface.h"
 
-namespace interface
+void TaskRunner(task::TaskEntry entry, std::string command, std::string argument)
 {
-	/*EntryManager
-		--
-		- table: HashTable<string, TaskEntry*>
-		-tree : HashTable<unsigned long long, TaskEntry*>
-		-history : OperationManager
-		--
-		+ void printAllTo(ostream& output);
-	+void createEntry(const string& description);
-	+void deleteEntry(const string& id);
-	+void updateEntryStatus(const string& id, const TaskEntryStatus& new_status);
-	+void updateEntryDescription(const string& id, const string& new_description);
-	+void updateEntryDue(const string& id, const string& new_time);
-	+void runDiagnosticTo(ostream& output);
-	+void loadEntry(
-		const string& user,
-		const string& id,
-		const uint64_t& time_created,
-		const uint64_t& time_due,
-		const string& description,
-		const TaskEntryStatus& status
-		);
-	+void unload();*/
-
-	void TaskRunner(task::TaskEntry entry, std::string command, std::string argument)
-	{
-		if (command == "create") {}
-		if (command == "delete") {}
-		if (command == "update_status") {}
-		if (command == "update_desc") {}
-		if (command == "update_due") {}
-		if (command == "test") {}
-		if (command == "load") {}
-	}
+	if (command == "create") {}
+	if (command == "delete") {}
+	if (command == "update_status") {}
+	if (command == "update_desc") {}
+	if (command == "update_due") {}
+	if (command == "test") {}
+	if (command == "load") {}
 }
---
+++ TaskApp/TaskInterface.h
@@ -0,0 +1,9 @@
+#ifndef TASK_INTERFACE_H
+#define TASK_INTERFACE_H
+#include "EntryManager.h"
+namespace taskapp
+{
+	void TaskRunner(task::TaskEntry entry, std::string command, std::string argument)
+}
+
+#endif
