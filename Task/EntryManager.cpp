f12f39143a77b11bcd7114c5f39c933c6e58ea11
mtso <mtso@tuta.io>
2016-12-03 12:33:03 -0800

    Remove namespace braces in EntryManager.cpp

--- Task/EntryManager.cpp
+++ Task/EntryManager.cpp
@@ -4,94 +4,94 @@
 
 #include "EntryManager.h"
 
-namespace task {
+using namespace task;
 
-	EntryManager::EntryManager()
-		: table(HashTable<string, TaskEntry>())
-		, tree_time_created(c_tree<uint64_t, TaskEntry>())
-		, history (adt::Stack<Operation>())
+EntryManager::EntryManager()
+	: table(HashTable<string, TaskEntry>())
+	, tree_time_created(c_tree<uint64_t, TaskEntry>())
+	, history (adt::Stack<Operation>())
+{
+}
+
+void EntryManager::loadTasklogs(vector<string> filenames)
+{
+	FileStore fileio;
+	string filepath;
+	vector<TaskEntry> entries;
+	TaskEntry* entry;
+
+	// Iterate through filenames
+	for (uint i = 0; i < filenames.size(); i++) 
 	{
-	}
+		filepath = "..\\.task\\" + filenames[i];
 
-	void EntryManager::loadTasklogs(vector<string> filenames)
-	{
-		FileStore fileio;
-		string filepath;
-		vector<TaskEntry> entries;
-		TaskEntry* entry;
-
-		// Iterate through filenames
-		for (uint i = 0; i < filenames.size(); i++) {
-			filepath = "..\\.task\\" + filenames[i];
-
-			// Return if the filepath was invalid
-			if (!fileio.load(filepath, entries)) { continue; }
+		// Return if the filepath was invalid
+		if (!fileio.load(filepath, entries)) { continue; }
 			
-			// Iterate through the entries loaded from fileio
-			for (uint count = 0; count < entries.size(); count++) 
-			{
-				entry = new task::TaskEntry(entries[count]);
-
-				table.insert(entry->getId(), *entry);
-				tree_time_created.insert(entry->getTimeCreatedMs(), entry);
-			}
-		}
-	}
-
-	void EntryManager::printAllTo(ostream& output)
-	{
-		TaskEntry* value;
-		for (uint64_t* key = tree_time_created.first_data(&value); key != NULL; key = tree_time_created.next_data(&value))
+		// Iterate through the entries loaded from fileio
+		for (uint count = 0; count < entries.size(); count++) 
 		{
-			output << "task " << value->getId() << endl;
-			output << "Due: " << value->getTimeDueStr() << endl << endl;
-			output << "\t" << value->getDescription() << endl << endl;
+			entry = new task::TaskEntry(entries[count]);
+
+			table.insert(entry->getId(), *entry);
+			tree_time_created.insert(entry->getTimeCreatedMs(), entry);
 		}
 	}
+}
 
-	void visitHistory(const Operation& operation)
+void EntryManager::printAllTo(ostream& output)
+{
+	TaskEntry* value;
+	for (uint64_t* key = tree_time_created.first_data(&value); key != NULL; key = tree_time_created.next_data(&value))
 	{
-		cout << operation.getType() << "\t" << operation.getPreviousState().getDescription() << endl;
+		output << "task " << value->getId() << endl;
+		output << "Due: " << value->getTimeDueStr() << endl << endl;
+		output << "\t" << value->getDescription() << endl << endl;
 	}
+}
 
-	void EntryManager::printHistoryTo(ostream& output)
+void visitHistory(const Operation& operation)
+{
+	cout << operation.getType() << "\t" << operation.getPreviousState().getDescription() << endl;
+}
+
+void EntryManager::printHistoryTo(ostream& output)
+{
+	history.traverse(visitHistory);
+}
+
+void EntryManager::createEntry(const string& description)
+{
+	//assign system time to create time
+	ULARGE_INTEGER int_time;
+	SYSTEMTIME sys_time;
+	GetSystemTime(&sys_time);
+	FILETIME file_time;
+	SystemTimeToFileTime(&sys_time, &file_time);
+	int_time.LowPart = file_time.dwLowDateTime;
+	int_time.HighPart = file_time.dwHighDateTime;
+	uint64_t createTime = (uint64_t)int_time.QuadPart;
+
+	//TaskEntry value("", description, createTime);
+	TaskEntryStatus new_status = BACKLOG;
+	TaskEntry* new_entry = new TaskEntry("mtso", description, createTime, createTime, new_status);
+
+	//tree_time_created.insert(createTime, &value);
+	history.push(Operation(CREATE, *new_entry));
+	tree_time_created.insert(createTime, new_entry);
+	table.insert(new_entry->getId(), *new_entry);
+}
+
+vector<TaskEntry> EntryManager::searchEntry(const string& search_term)
+{
+	vector<TaskEntry> found;
+	TaskEntry* value;
+	for (uint64_t* key = tree_time_created.first_data(&value); key != NULL; key = tree_time_created.next_data(&value))
 	{
-		history.traverse(visitHistory);
-	}
-
-	void EntryManager::createEntry(const string& description)
-	{
-		//assign system time to create time
-		ULARGE_INTEGER int_time;
-		SYSTEMTIME sys_time;
-		GetSystemTime(&sys_time);
-		FILETIME file_time;
-		SystemTimeToFileTime(&sys_time, &file_time);
-		int_time.LowPart = file_time.dwLowDateTime;
-		int_time.HighPart = file_time.dwHighDateTime;
-		uint64_t createTime = (uint64_t)int_time.QuadPart;
-
-		//TaskEntry value("", description, createTime);
-		TaskEntryStatus new_status = BACKLOG;
-		TaskEntry* new_entry = new TaskEntry("mtso", description, createTime, createTime, new_status);
-
-		//tree_time_created.insert(createTime, &value);
-		history.push(Operation(CREATE, *new_entry));
-		tree_time_created.insert(createTime, new_entry);
-		table.insert(new_entry->getId(), *new_entry);
-	}
-
-	vector<TaskEntry> EntryManager::searchEntry(const string& search_term)
-	{
-		vector<TaskEntry> found;
-		TaskEntry* value;
-		for (uint64_t* key = tree_time_created.first_data(&value); key != NULL; key = tree_time_created.next_data(&value))
+		if (value->getDescription().find(search_term) != string::npos)
 		{
-			if (value->getDescription().find(search_term) != string::npos)
-			{
-				found.push_back(*value);
-			}
+			found.push_back(*value);
 		}
-		return found;
 	}
+	return found;
 }
