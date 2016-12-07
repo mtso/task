64bdd0348389f235d3b0272f028221b361d00126
mtso <mtso@tuta.io>
2016-12-07 10:26:52 -0800

    Add comments

--- Task/EntryManager.h
+++ Task/EntryManager.h
@@ -28,26 +28,37 @@ namespace task
 	class EntryManager 
 	{
 	private:
+		// Used to differentiate tasklog file for input/output
 		string current_user;
 
+		// Used to determine where the .task folder is located (top-level or ..\\ one level over)
 		string root_directory;
 
+		// The HashTable
 		HashTable<string, TaskEntry> table;
 
+		// The BST sorted by creation time 64bit int
 		c_tree<uint64_t, TaskEntry> tree_time_created;
 
+		// Stores user operations of each session in Last In First Out fashion
 		adt::Stack<Operation> history;
 
+		// Outputs the operation history by traversing the stack
 		friend void printHistory(const Operation& operation);
 
+		// Outputs the table order by traversing the HashTable
 		friend void printTable(TaskEntry& entry);
 
+		// Outputs the entries in hash table order to a file named "hashtable_output.txt"
 		friend void saveHashTable(TaskEntry& entry);
 
+		// Inserts a TaskEntry object into the table and tree
 		void insertEntry(const TaskEntry& entry);
 
+		// Prints an entry's unique id, creator's username, due date time, and description to an output stream
 		void printEntryTo(ostream& output, const TaskEntry* entry);
 
+		// Used to throw exceptions if the search terms come up empty
 		class NotFoundException {
 		private:
 			string description;
@@ -57,64 +68,92 @@ namespace task
 		};
 
 	public:
-
+		// Default constructor, no longer used
 		EntryManager();
 
+		// Constructor accepting username and root directory argument
 		EntryManager(const string& user, const string& directory);
 
+		// Sets the username
 		void setCurrentUser(const string& user);
 
 		void setRootDirectory(const string& directory);
 
+		//===============================
 		// Task output listing functions
+		////===============================
 
+		// Prints all entries from all users
 		void printAllTo(ostream& output);
 
+		// Prints just the user's backlog and in-progress tasks to output
+		// by tree order
 		void printUserTasksTo(ostream& output);
-
+		
+		// Prints all of the user's tasks by tree order
 		void printUserTasksAllTo(ostream& output);
 
+		// Outputs by HashTable order
 		void printTableTo(ostream& output);
 
+		// Outputs an indented tree representation of the BST
 		void printTreeTo(ostream& output);
 		
-		/**
-		 * Prints a list of the operation history to cout.
-		 */
+		// Prints a list of the operation history to cout.
 		void printHistoryTo(ostream& output);
 
+		// Creates a new entry from the description
+		// with a default of backlog status and no due date time
+		// and then calls insertEntry to add to table and tree
 		void createEntry(const string& description);
 
+		// Searches the description of all entries with a search term
+		// by traversing the BST
 		vector<TaskEntry> searchEntryDescription(const string& search_term);
 
+		// Returns an entry by full id
 		TaskEntry getEntryById(const string& id);
 
+		// Attempts to match a fragment of an id to a full id
+		// Will return false if no entries are found
+		// OR more than one entry is matched
 		bool getFullIdFor(const string& id, string& found_id);
 
+		// Deletes and entry by id from table and tree
 		bool deleteEntry(const string& id);
 
+		// Updates an entry's status, records the operation to the history
 		bool updateEntryStatus(const string& id, const TaskEntryStatus& new_status);
 
+		// Updates an entry's description, records the operation to the history
 		bool updateEntryDescription(const string& id, const string& new_description);
         
+		// Updates an entry's due date, records the operation to the history
 		bool updateEntryDueDate(const string& id, uint64_t new_time_due);
 
-		//bool updateEntryDueAdd(const string& id, const string& add_time);
-
+		// Runs the diagnostic of table and tree and prints it to an output stream
 		void runDiagnosticTo(ostream& output, const int& run_count = 1000);
 
+		// Prints the current stats of the hash table
 		void printCurrentStateTo(ostream& output);
 
-		// used upon quitting the app
+		// outputs the user's tasks to his/her tasklog file
+		// Deletion of other user's entries does not get saved to their tasklogs
+		// outputs a hashtable_output.txt file of the HashTable order
 		void unload();
 
+		// Pops the top operation from the history stack
+		// and reverts the TaskEntry to its previous state
 		void undoTopOperation(ostream& output);
 
+		// Loads entries from a vector of filenames on startup
 		void loadTasklogs(vector<string> filenames);
 
 		// !DANGER!
+		// Deletes all entries
 		void clear();
 
+		// Adds dummy entries into the table and tree for demo
 		void fill(const int& count);
 	};
 }
--- Task/HashTable.h
+++ Task/HashTable.h
@@ -35,21 +35,30 @@ namespace task
 		static const uint HASH_START = 15607; // 701;
 		static const uint HASH_MULTIPLIER = 17;
 
+		// Points to the dynamic array of linked lists
 		HashList<KV>* table;
 
 		unsigned int entry_count;
-
 		unsigned int used_tables;
 		unsigned int table_size;
 		unsigned int index_of_longest;
 		unsigned int collisions;
 
+		// Uses a multiplicative hash to determine
+		// an index based off of current table size
 		inline unsigned int hashOf(const KeyType& target_key, const unsigned int &for_size) const;
 
+		// Re-hashes the table to decrease load factor
+		// will often lower the count of the longest list too 
 		void expandTable();
 
+		// Checks the primality of a number
 		bool isPrime(const unsigned int &number);
+
+		// Returns the next prime number greater than a minimum
 		unsigned int nextPrime(unsigned int minimum);
+
+		// Forces an expansion of the table.
 		void forceExpansion();
 
 	public:
@@ -64,7 +73,6 @@ namespace task
 
 		/**
 		 * Returns true if the Dictionary is empty
-		 *
 		 */
 		bool isEmpty() const;
 
@@ -79,46 +87,53 @@ namespace task
 		bool insert(const KeyType& new_key, const ValueType& new_value);
 
 		/**
-		 *
+		 * Removes an item by its key, may throw if entry is not found
 		 */
 		bool remove(const KeyType& delete_key);
 
 		/**
-		*
+		* Clears each list in the table
 		*/
 		void clear();
 
 		/**
-		*
+		* Returns a copy of the value of an entry identified by key
 		*/
 		ValueType getValue(const KeyType& target_key) const;
 
 		/**
-		 *
+		 * Returns a reference to the value of an entry identified by the key
 		 */
 		ValueType& getRawValue(const KeyType& target_key);
 
 		/**
-		*
+		* Returns true if an entry is found with a key that matches
 		*/
 		bool contains(const KeyType& target_key) const;
 
 		/**
-		*
+		* Traverses each list in the table by array order
+		* the list traverses from top to bottom
 		*/
 		virtual void traverse(void visit( ValueType& entry)) const;
 
-
+		// Returns the load factor between 0 and 1,
+		// number of non-empty tables divided by table size
 		double getLoadFactor() const;
 
+		// Returns the longest list identfied by index
 		unsigned int countLongestList() const;
 
+		// Returns the number of non-empty tables
 		unsigned int countUsedTables() const;
 
+		// Returns the current table size, should be a prime number
 		unsigned int getTableSize() const;
 
+		// Returns the number of comparisons before finding an entry by key
 		int getAccessCountOf(const KeyType& target_key);
 
+		// Returns the number of tables with more than one entry in it
 		uint getCollisionCount() const;
 	};
 }
