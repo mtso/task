24fffdbc74fbb57d2f8c0018a10a07a570799de3
mtso <mtso@tuta.io>
2016-12-06 20:26:05 -0800

    Edit hash values

--- Task/HashTable.h
+++ Task/HashTable.h
@@ -32,7 +32,7 @@ namespace task
 	{
 	private:
 		static const uint DEFAULT_SIZE = 101; // Should be a prime number
-		static const uint HASH_START = 593; // 701;
+		static const uint HASH_START = 15607; // 701;
 		static const uint HASH_MULTIPLIER = 17;
 
 		HashList<KV>* table;
